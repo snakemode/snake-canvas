455151d2ea864c094ac4c195aa1739f612217cac
David Whitney <david@davidwhitney.co.uk>
2020-08-10 17:18:22 +0100

    Better demo in repo.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import { DrawableCanvasElement } from "./DrawableCanvasElement.js";
 
-const canvas = new DrawableCanvasElement("foo").onNotification((evt) => console.log(evt));
+const canvas = new DrawableCanvasElement("foo").registerPaletteElements("paletteId").onNotification((evt) => console.log(evt));
